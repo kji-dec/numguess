611c989d40c5c69902007bbb0a7fd1c3a9a19d65
kji-dec <dec22sag@gmail.com>
2023-01-09 16:17:37 +0900

    feat: Get user's input - name, answer
    First, system gets user's name. After that, system says hello to user.
    System asks user to guess random number. If user enter the numner,
    system repeats user's answer.
    
    resolved #1

---
+++ numguess.py
@@ -0,0 +1,13 @@
+from random import randint
+
+# Get user name
+user = input("What\'s your name?\n")
+
+# Say hello to user
+print(f"Hello, {user}!")
+
+# Get user's guessing gnumber
+guess = input("Guess random number : ")
+
+#Print user's guess
+print(f"You guessed {guess} as random number!")
