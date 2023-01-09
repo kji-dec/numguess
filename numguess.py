369eabcdfb7c3e124ba4aaec7dafe0dd927f33f1
kji-dec <dec22sag@gmail.com>
2023-01-09 16:26:27 +0900

    feat: Make and print random number
    System makes random integer by randint function. Put the radint into
    answer variable, and print the answer.
    
    resolves #2

--- numguess.py
+++ numguess.py
@@ -1,5 +1,8 @@
 from random import randint
 
+#Generate random number
+answer = randint(1, 100)
+
 # Get user name
 user = input("What\'s your name?\n")
 
@@ -9,5 +12,8 @@ print(f"Hello, {user}!")
 # Get user's guessing gnumber
 guess = input("Guess random number : ")
 
-#Print user's guess
+# Print user's guess
 print(f"You guessed {guess} as random number!")
+
+# Print answer
+print(f"The random number was {answer}!")
