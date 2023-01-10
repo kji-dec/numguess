72da9f98d1ef0be5e055a62bd01593a5da1b1b73
kji-dec <dec22sag@gmail.com>
2023-01-10 15:11:21 +0900

    feat: Compare user's guess and answer
    if user's guess is right, print greeting message.
    else, print wrong message to user.

--- numguess.py
+++ numguess.py
@@ -1,4 +1,5 @@
 from random import randint
+from time import sleep
 
 #Generate random number
 answer = randint(1, 100)
@@ -15,5 +16,14 @@ guess = input("Guess random number : ")
 # Print user's guess
 print(f"You guessed {guess} as random number!")
 
-# Print answer
-print(f"The random number was {answer}!")
+# Compare answer with user's guess
+if guess == answer:
+    print("**************************")
+    sleep(1)
+    print("**************************")
+    sleep(1)
+    print("**************************")
+    sleep(1)
+    print(f"You got the right answer! the answer was {answer}!")
+else:
+    print(f"~~XD~~ You got the wrong answer~~ the answer was {answer} :-P")
