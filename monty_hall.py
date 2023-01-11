a06c6b454380cfb56104249e24c4fd6523d12572
kji-dec <dec22sag@gmail.com>
2023-01-11 09:58:07 +0900

    feat: add doing simulation
    by using for loop, I made simulations.
    also, made result_win dictionary to save the winning times.

--- monty_hall.py
+++ monty_hall.py
@@ -3,7 +3,16 @@ from random import shuffle, choice
 simul = 1000
 
 door = [0, 0, 1]
+result_win = {"changed": 0, "not_changed": 0}
 
 for _ in range(simul):
     shuffle(door)
     user_choice = choice(door)
+
+    # user changed choice
+    if user_choice == 0:
+        result_win["changed"] += 1
+
+    # user didn't change choice
+    if user_choice == 1:
+        result_win["not_changed"] += 1
