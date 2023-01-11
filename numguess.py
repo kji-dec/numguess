c06cbe54fbc004a767112128bbc35fcbc3733816
kji-dec <dec22sag@gmail.com>
2023-01-11 16:17:00 +0900

    feat: divide into functions
    divided functions by features

--- numguess.py
+++ numguess.py
@@ -1,34 +1,49 @@
 from random import randint
 from time import sleep
 
-#Generate random number
-answer = randint(1, 100)
 
-print(answer)
+def gen_answer(start, end):
+    # Generate random number
+    answer = randint(start, end + 1)
 
-# Get user name
-user = input("What\'s your name?\n")
+    print(answer)
 
-# Say hello to user
-print(f"Hello, {user}!")
+def get_user() -> str:
+    # Get user name
+    user = input("What\'s your name?\n")
 
-# Get user's guessing gnumber
-guess = int(input("Guess random number(1-100) : "))
+    # Say hello to user
+    print(f"Hello, {user}!")
 
-# Print user's guess
-print(f"You guessed {guess} as random number!")
+    return user
 
-# Compare answer with user's guess
-if guess == answer:
-    print("**************************")
-    sleep(1)
-    print("**************************")
-    sleep(1)
-    print("**************************")
-    sleep(1)
-    print(f"You got the right answer! the answer was {answer}!")
-# To give hint to user, compare which is larger (guess, answer)
-elif guess > answer:
-    print("Your guess is larger than the answer")
-else:
-    print("Your guess is smaller than the answer")
+def guess_number() -> int:
+    # Get user's guessing number
+    guess = int(input("Guess random number(1-100) : "))
+
+    # Print user's guess
+    print(f"You guessed {guess} as random number!")
+
+    return guess
+
+def judge(guess, answer):
+    # Compare answer with user's guess
+    if guess == answer:
+        print("**************************")
+        sleep(1)
+        print("**************************")
+        sleep(1)
+        print("**************************")
+        sleep(1)
+        print(f"You got the right answer! the answer was {answer}!")
+    # To give hint to user, compare which is larger (guess, answer)
+    elif guess > answer:
+        print("Your guess is larger than the answer")
+    else:
+        print("Your guess is smaller than the answer")
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
